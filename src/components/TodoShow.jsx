42419ba82826ebb1b63a0acd9f4c78a8c895537f
Mehmet ÜLKER <mehmet-ulker@hotmail.com>
2024-07-12 00:14:12 +0300

    TodoShow propsTypes defined.

--- src/components/TodoShow.jsx
+++ src/components/TodoShow.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export default function TodoShow({ index, item }) {
   return (
     <div>
-      <li key={index}>{index} {item.text}</li>
+      <li key={index}>
+        {index} {item.text}
+      </li>
     </div>
   );
 }
+TodoShow.propTypes = {
+  index: PropTypes.number.isRequired,
+  item: PropTypes.object.isRequired,
+};
